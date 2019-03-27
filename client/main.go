3ffc51ca948a08cc982c6a5ed3d9d30d9607a526
Andy Cobaugh <andrew.cobaugh@gmail.com>
2019-03-27 00:38:13 -0400

    define send() function in bluecat client

--- client/main.go
+++ client/main.go
@@ -4,6 +4,10 @@ import (
 	"net/url"
 	"net/http"
 	"crypto/tls"
+	"fmt"
+	"bytes"
+	"io/ioutil"
+	"encoding/json"
 )
 
 type Config struct {
@@ -18,7 +22,7 @@ type Client struct {
 	UserAgent string
 
 	URL *url.URL
-	client *http.Client
+	hc *http.Client
 	token string
 }
 
@@ -34,7 +38,46 @@ func NewClient(config *Config) (*Client, error) {
 		return nil, err
 	}
 
-	c := &Client{Config: config, UserAgent: "", URL: url, client: httpClient}
+	c := &Client{Config: config, UserAgent: "", URL: url, hc: httpClient}
 
 	return c, nil
+}
+
+// request accepts a HTTP method, resource, payload, and query parameters and returns an *http.Response
+func (c *Client) request(method string, resource string, payload interface{}, query map[string]string) (*http.Response, error) {
+	url := fmt.Sprintf("%s/Services/REST/v1/%s", c.Config.BaseURL, resource)
+
+	body, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(method, url, bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	if len(c.token) > 0 {
+		req.Header.Set("Authorization", c.token)
+	}
+
+	q := req.URL.Query()
+	for argName, argVal := range query {
+		q.Add(argName, argVal)
+	}
+	req.URL.RawQuery = q.Encode()
+	resp, err := c.hc.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode >= 400 {
+		errBytes, _ := ioutil.ReadAll(resp.Body)
+		errResp := string(errBytes)
+		return nil, fmt.Errorf("bluecat: request failed with HTTP status code %d\n Full message: %s",
+			resp.StatusCode, errResp)
+	}
+
+	return resp, nil
 }
